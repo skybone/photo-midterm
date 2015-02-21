7a4101daca9eff553dee73e0fe419156008033ca
Taylor Smith <taylor.smith84@gmail.com>
2015-02-21 10:25:07 -0500

    simplified the mail scrip added ob_start to fix header issue

--- mail.php
+++ mail.php
@@ -1,17 +1,18 @@
-
-
-<?php $name = $_POST['name'];
+<?php 
+    ob_start();
+    $name = $_POST['name'];
     $email = $_POST['email'];
     $text = $_POST['message'];
     $subject = "Email submission from Skylyy.com";
     $emailto = "slyyti@gmail.com";
-    $header = "From: $name <$email>\r\nReply-To: $email\r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-type:text/html;charset=iso-8859-1\r\n";
+    $headers = "From: $name <$email>\r\nReply-To: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type:text/html;charset=iso-8859-1\r\n";
     $message = "From: $name, Email: $email<br /><hr />$text";
-    mail($emailto, $subject, $message, $header);
-
+    mb_send_mail($emailto, $subject, $message, $headers);
+    ob_flush();
     header("Location: index.php");
+
 ?>
 
              
